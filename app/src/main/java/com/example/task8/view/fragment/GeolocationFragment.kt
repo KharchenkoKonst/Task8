374dfdc0d30b392598b1db50dc48b11b10cf16fc
KharchenkoKA <kharshenko.kos@gmail.com>
2021-08-05 12:54:42 +0300

    Рабочая геолокация

--- app/src/main/java/com/example/task8/view/fragment/GeolocationFragment.kt
+++ app/src/main/java/com/example/task8/view/fragment/GeolocationFragment.kt
@@ -1,71 +1,121 @@
 package com.example.task8.view.fragment
 
 import android.Manifest
-import android.app.Activity
+import android.annotation.SuppressLint
+import android.content.Context.LOCATION_SERVICE
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
-import android.util.Log
+import android.os.Looper
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import androidx.fragment.app.viewModels
-import com.example.task8.LocationService
 import com.example.task8.databinding.FragmentGeolocationBinding
-import com.example.task8.viewmodel.GeoViewModel
-import com.example.task8.viewmodel.GeoViewModelFactory
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 
 class GeolocationFragment : Fragment() {
     private lateinit var binding: FragmentGeolocationBinding
 
-    private val viewModelFactory: GeoViewModelFactory by lazy { GeoViewModelFactory() }
-    private val viewModel: GeoViewModel by viewModels { viewModelFactory }
+//    private val viewModelFactory: GeoViewModelFactory by lazy { GeoViewModelFactory() }
+//    private val viewModel: GeoViewModel by viewModels { viewModelFactory }
+
+
+    lateinit var locationService: LocationService
+    private val ID_LOCATION_PERMISSION = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentGeolocationBinding.inflate(layoutInflater)
-//        requirePermissions()
-        init()
-//        subscribeToVM()
+        locationService = LocationService().apply { getLastLocation() }
         return binding.root
     }
 
-    private lateinit var locationClient: LocationService
-
-    private fun init() {
-        locationClient = LocationService(requireContext())
-        viewModel.setLocationService(locationClient)
-
-        viewModel.location.observe(viewLifecycleOwner, {
-            val pos = "${it.longitude} : ${it.latitude}"
-            Log.i("location", pos)
-            binding.geolocationTextView.text = pos
-        })
-//        locationClient.lastLocation.addOnSuccessListener { location: Location? ->
-//            binding.geolocationTextView.text = "${location?.latitude} : ${location?.longitude}"
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == ID_LOCATION_PERMISSION) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                locationService.getLastLocation()
+            }
+        }
     }
 
-//        viewModel.setLocationService(LocationService(requireContext()))
 
-//    private fun subscribeToVM() {
-//        viewModel.LocationData.getCurrentPosition().observe(viewLifecycleOwner, {
-//            Log.i("observer", it.provider)
-//            binding.geolocationTextView.text = "${it.longitude} : ${it.latitude}"
-//        })
-//    }
+    inner class LocationService {
 
-//    private fun requestPermissions() {
-//        val permissions = listOf(
-//            Manifest.permission.ACCESS_COARSE_LOCATION,
-//            Manifest.permission.ACCESS_FINE_LOCATION
-//        )
-//        ActivityCompat.requestPermissions(requireActivity(), permissions.toTypedArray(), 0)
-//    }
+        private val fusedLocationClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
 
+        @SuppressLint("MissingPermission")
+        fun getLastLocation() {
+            if (checkPermissions()) {
+                if (isLocationEnabled()) {
+
+                    fusedLocationClient.requestLocationUpdates(
+                        LocationRequest.create().apply {
+                            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                            interval = 0
+                            fastestInterval = 0
+                            numUpdates = 1
+                        },
+                        object : LocationCallback() {
+                            override fun onLocationResult(p0: LocationResult) {
+                                binding.geolocationTextView.text =
+                                    "${p0.lastLocation.latitude} : ${p0.lastLocation.longitude}"
+                            }
+                        }, Looper.getMainLooper()
+                    )
+                } else {
+                    Toast.makeText(requireContext(), "Turn on location", Toast.LENGTH_LONG).show()
+                    val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    startActivity(intent)
+                }
+            } else {
+                requestPermissions()
+            }
+        }
+
+        private fun isLocationEnabled(): Boolean {
+            val locationManager: LocationManager =
+                requireContext().getSystemService(LOCATION_SERVICE) as LocationManager
+            return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+            )
+        }
+
+        private fun checkPermissions(): Boolean {
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                return true
+            }
+            return false
+        }
+
+        private fun requestPermissions() {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                ID_LOCATION_PERMISSION
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/task8/viewmodel/GeoViewModel.kt
+++ app/src/main/java/com/example/task8/viewmodel/GeoViewModel.kt
@@ -1,16 +1,13 @@
 package com.example.task8.viewmodel
 
-import android.location.Location
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.example.task8.LocationService
 
 class GeoViewModel : ViewModel() {
-    private lateinit var locationData: LocationService
-    lateinit var location: LiveData<Location>
-
-    fun setLocationService(locationService: LocationService) {
-        this.locationData = locationService
-        location = locationData.getCurrentPosition()
-    }
+//    private lateinit var locationData: LocationService
+//    lateinit var location: LiveData<Location>
+//
+//    fun setLocationService(locationService: LocationService) {
+//        this.locationData = locationService
+//        location = locationData.getCurrentPosition()
+//    }
 }
